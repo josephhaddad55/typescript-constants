6efd0a0719df0cf849e20a3d55247cafa9de0046
Joe <33713907+josephhaddad55@users.noreply.github.com>
2019-06-25 13:48:32 -0400

    Created months.ts

---
+++ months.ts
@@ -0,0 +1,14 @@
+export const months = [
+  { id: 1, long_name: 'January', short_name: 'Jan', code: 'jan' },
+  { id: 2, long_name: 'February', short_name: 'Feb', code: 'feb' },
+  { id: 3, long_name: 'March', short_name: 'Mar', code: 'mar' },
+  { id: 4, long_name: 'April', short_name: 'Apr', code: 'apr' },
+  { id: 5, long_name: 'May', short_name: 'May', code: 'may' },
+  { id: 6, long_name: 'June', short_name: 'Jun', code: 'jun' },
+  { id: 7, long_name: 'July', short_name: 'Jul', code: 'jul' },
+  { id: 8, long_name: 'August', short_name: 'Aug', code: 'aug' },
+  { id: 9, long_name: 'September', short_name: 'Sep', code: 'sept' },
+  { id: 10, long_name: 'October', short_name: 'Oct', code: 'oct' },
+  { id: 11, long_name: 'November', short_name: 'Nov', code: 'nov' },
+  { id: 12, long_name: 'December', short_name: 'Dec', code: 'dec' },
+];
